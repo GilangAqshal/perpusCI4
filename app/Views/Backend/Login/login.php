e842eaca35b6317c1339e7bdba42a0ac74f9caa4
GilangAqshal <gilangais16@gmail.com>
2026-05-27 22:00:32 +0700

    Update login

--- app/Views/Backend/Login/login.php
+++ app/Views/Backend/Login/login.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>USAUMEME</title>
+<title>LOGIN</title>
 
 <link href="/Assets/css/bootstrap.min.css" rel="stylesheet">
 <link href="/Assets/css/datepicker3.css" rel="stylesheet">
